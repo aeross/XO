9536f4d516a2481506a9ee32793d9e0a4ca5495f
aeross <andrewpratama22@gmail.com>
2023-03-04 16:47:20 +1100

    Improved commenting

--- Main.java
+++ Main.java
@@ -1,53 +1,37 @@
 import java.util.Scanner;
 
+
 public class Main {
-
     public static void main(String[] args) {
-        // 1: BUILDING THE BOARD
-        // call an object from BuildBoard class
-        BuildBoard gameBoard = new BuildBoard();
-
-        // build the tic tac toe board
-        char board[][] = gameBoard.buildBoard();
-
-        // as the first step, print the empty board
-        gameBoard.printBoard(board);
-
+        // 1: BUILD THE BOARD
+        Board gameBoard = new Board();
+        char XOboard[][] = gameBoard.buildBoard();
+        gameBoard.printBoard(XOboard);
 
         // 2: PLAYER INPUT
-        // after printing the empty board, the program then prompts
-        // the players to make a move.
-        // note: the general convention is first player is always 'X', and the second player 'O'.
-
-        // a question about this might be raised: how is the player going to input?
-        // and how is the program going to interpret the input?
-
-        /**
-        to keep things simple, the player simply has to type a number between 1 to 9,
-        with each number representing these locations on the board:
-
-         1 | 2 | 3 
-        ___|___|___
-         4 | 5 | 6 
-        ___|___|___
-         7 | 8 | 9 
-           |   |   
-
-        for example, if the player inputs number 7, the board would look like this:
-
-           |   |   
-        ___|___|___
-           |   |   
-        ___|___|___
-         X |   |   
-           |   |   
-        
-        After getting the input, we first have to check whether it is valid. Checking for
-        the first player's move is easy -- we simply just have to check whether the input is
-        an integer from 1 to 9 inclusive. But the move following that, we need to also check
-        that there are no repeated moves, i.e. if a player has already picked 3, 3 cannot be
-        picked again by either player.
-        **/
+        /* after printing the empty board, the program then prompts the players to make a move.
+         * note: the general convention is first player is always 'X', and the second player 'O'.
+         * 
+         * to keep things simple, the player simply has to type a number between 1 to 9,
+         * with each number representing these locations on the board:
+         * 
+         *  1 | 2 | 3 
+         * ___|___|___
+         *  4 | 5 | 6 
+         * ___|___|___
+         *  7 | 8 | 9 
+         *    |   |   
+         * 
+         * for example, if the first player inputs number 7, the board would look like this:
+         * 
+         *    |   |   
+         * ___|___|___
+         *    |   |   
+         * ___|___|___
+         *  X |   |   
+         *    |   |   
+         * 
+         */
 
 
         PlayerInput gameInput = new PlayerInput();
@@ -63,17 +47,16 @@ public class Main {
             validMoves = gameInput.updateValidMoves(move, validMoves);
 
             // update and print board
-            board = gameBoard.updateBoard(player, move, board);
+            XOboard = gameBoard.updateBoard(player, move, XOboard);
             System.out.println("");  // line skip for formatting purposes
-            gameBoard.printBoard(board);
+            gameBoard.printBoard(XOboard);
 
 
             // 3: CHECK WIN CONDITION
-            // The last step to finishing the program is to check win condition.
             // Whenever there are three X's or O'x in a row, horizontally, diagonally,
-            // or vertically, that player has won.
+            // or vertically, that player wins
             WinCondition gameWin = new WinCondition();
-            if (gameWin.checkWin(board)) {
+            if (gameWin.checkWin(XOboard)) {
                 System.out.println("Player " + player + " victory!");
                 break;
             }
@@ -92,3 +75,8 @@ public class Main {
         // the end!
     }
 }
+
+
+
+
+
