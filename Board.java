cdd19d14d5eed8adad86de4dfeb5dbc1338e10c9
aeross <andrewpratama22@gmail.com>
2023-03-05 23:04:26 +1100

    fleshed out code to be in a more OOP way

--- Board.java
+++ Board.java
@@ -1,5 +1,6 @@
 /**
- * A tic tac toe board is 3x3 which looks like this:
+ * Board implementation w/o GUI, which relies on typed user input to set/update the board values.
+ * As an example, a tic tac toe board is 3x3 which looks like this:
  * 
  *    |   |   
  * ___|___|___
@@ -24,95 +25,118 @@
  * [2,0] | [2,1] | [2,2]
  *       |       |       
  * 
- * A finished (tied) game would look something like this:
+ * To keep things simple (and avoid two-dimensional confusion), I choose for 
+ * the naming system of each cell to be a number in ascending order starting from 1.
+ * For example, a 3x3 board has numbers from 1 to 9,
+ * with each number representing these locations on the board:
  * 
- *  X | O | X 
+ *  1 | 2 | 3 
  * ___|___|___
- *  O | X | O 
+ *  4 | 5 | 6 
  * ___|___|___
- *  O | X | X 
+ *  7 | 8 | 9 
  *    |   |   
  * 
+ * 
+ * Note: this Board class assumes a square board (equal number of rows and columns)
  **/
 
- public class Board {
-    private int size = 3;
+import java.util.Arrays;
 
-    char[][] buildBoard() {
-        // builds a board with 2d array as explained above
-        char board[][] = new char[size][size];
+public class Board {
+    private int size;
+    
+    // constructor
+    Board() {
+        this.size = 0;
+    }
+
+    Board(int size) {
+        this.size = size;
+    }
+
+    // methods
+    public char[][] buildBoard() {
+        // builds a board with 2d array as explained above and fills it with ' '
+        char board[][] = new char[this.size][this.size];
+        for (char[] col: board) {
+            Arrays.fill(col, ' ');
+        }
         return board;
     }
 
-    void printBoard(char[][] board) {
+    public void printBoard(char[][] board) {
         // prints the board with the same format as explained above
         int colnum = 0, rownum;
         for (char[] col: board) {
-            rownum = 0;
 
+            // prints the upper part of a column
+            rownum = 0;
             for (char i: col) {
-                if (i == 'X' || i == 'O') {
-                    System.out.print(" " + i + " ");
-                } else {
-                    System.out.print("   ");
-                }
+                System.out.print(" " + i + " ");
 
                 rownum += 1;
-                if (rownum < size) {
+                if (rownum < this.size) {
                     System.out.print("|");
                 }
             }
-
+            
+            // prints the lower part of a column
+            rownum = 1;
             colnum += 1;
-            if (colnum < size) {
-                System.out.println("\n___|___|___");
+            if (colnum < this.size) {
+                System.out.print("\n___");
+                while (rownum < this.size) {
+                    rownum += 1;
+                    System.out.print("|___");
+                }
             } else {
-                System.out.println("\n   |   |   ");
+                System.out.print("\n   ");
+                while (rownum < this.size) {
+                    rownum += 1;
+                    System.out.print("|   ");
+                }
             }
+            System.out.print("\n");
         }
     }
 
-    char[][] updateBoard(int player, int move, char[][] board) {
-        // updates the board based on player move.
+    char[][] updateBoard(char newVal, int loc, char[][] board) {
+        // updates the value of the board
+        int length = this.size;
+
+        int colLoc = (loc - 1) / length;
+        int rowLoc = (loc - 1) % length;
+        board[colLoc][rowLoc] = newVal;
+
+        return board;
+    }
+}
+
+
+
+class XOBoard extends Board {
+    // XO, another name for tic tac toe
+
+    private static final int XOSIZE = 3;
+
+    XOBoard() {
+        super(XOSIZE);
+    }
+
+    char[][] updateXOBoard(int player, int move, char[][] board) {
+        // updates tic tac toe board based on player move.
         // move is an integer from 1 to 9 corresponding to the location of the board,
-        // player is an integer, either 1 or 2.
+        // player is an integer, either 1 or 2, with player 1 corresponding to 'X' and 2 to 'O'.
 
-        char symbol;
+        char newVal;
         if (player == 1) {
-            symbol = 'X';
+            newVal = 'X';
         } else {
-            symbol = 'O';
+            newVal = 'O';
         }
 
-        switch(move) {
-            case 1:
-                board[0][0] = symbol;
-                break;
-            case 2:
-                board[0][1] = symbol;
-                break;
-            case 3:
-                board[0][2] = symbol;
-                break;
-            case 4:
-                board[1][0] = symbol;
-                break;
-            case 5:
-                board[1][1] = symbol;
-                break;
-            case 6:
-                board[1][2] = symbol;
-                break;
-            case 7:
-                board[2][0] = symbol;
-                break;
-            case 8:
-                board[2][1] = symbol;
-                break;
-            case 9:
-                board[2][2] = symbol;
-                break;
-        }
+        board = super.updateBoard(newVal, move, board);
         return board;
     }
 }
