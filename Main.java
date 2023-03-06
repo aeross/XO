b9a6a3b094fb32de8338eb798a1729be1606a1c2
aeross <andrewpratama22@gmail.com>
2023-03-06 16:02:37 +1100

    Bug fixes

--- Main.java
+++ Main.java
@@ -4,59 +4,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 1: BUILD THE BOARD
-        Board gameBoard = new Board();
-        char XOboard[][] = gameBoard.buildBoard();
-        gameBoard.printBoard(XOboard);
+        XOBoard gameBoard = new XOBoard();
+        char board[][] = gameBoard.buildBoard();
+        gameBoard.printBoard(board);
 
-        // 2: PLAYER INPUT
-        /* after printing the empty board, the program then prompts the players to make a move.
-         * note: the general convention is first player is always 'X', and the second player 'O'.
-         * 
-         * to keep things simple, the player simply has to type a number between 1 to 9,
-         * with each number representing these locations on the board:
-         * 
-         *  1 | 2 | 3 
-         * ___|___|___
-         *  4 | 5 | 6 
-         * ___|___|___
-         *  7 | 8 | 9 
-         *    |   |   
-         * 
-         * for example, if the first player inputs number 7, the board would look like this:
-         * 
-         *    |   |   
-         * ___|___|___
-         *    |   |   
-         * ___|___|___
-         *  X |   |   
-         *    |   |   
-         * 
-         */
-
-
-        PlayerInput gameInput = new PlayerInput();
-        
+        // 2: PLAYER MOVE
+        XOPlayerInput gameInput = new XOPlayerInput();
         // we are going to use this array to check for valid moves 
-        int validMoves[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int validMoves[] = gameInput.getValidMoves();
         // and this to determine whether it's player 1 or player 2's turn
-        int player = 1;
-
+        int player = gameInput.getPlayer();
+        
+        
         while (validMoves.length > 0) {
             // get input    
-            int move = gameInput.getInput(player, validMoves);
+            int move = gameInput.getInput(validMoves, player);
+
+            // update valid moves
             validMoves = gameInput.updateValidMoves(move, validMoves);
 
             // update and print board
-            XOboard = gameBoard.updateBoard(player, move, XOboard);
+            board = gameBoard.updateXOBoard(player, move, board);
             System.out.println("");  // line skip for formatting purposes
-            gameBoard.printBoard(XOboard);
-
+            gameBoard.printBoard(board);
 
             // 3: CHECK WIN CONDITION
-            // Whenever there are three X's or O'x in a row, horizontally, diagonally,
-            // or vertically, that player wins
             WinCondition gameWin = new WinCondition();
-            if (gameWin.checkWin(XOboard)) {
+            if (gameWin.checkWin(board)) {
                 System.out.println("Player " + player + " victory!");
                 break;
             }
